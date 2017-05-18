5d6d4a3c156237ebfdbaa1f161f839af4efd632e
Tvde1 <tim3345@live.nl>
2017-05-18 09:44:59 +0200

    Add project files.

---
+++ OdoriRails.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OdoriRails.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ OdoriRails/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OdoriRails.MvcApplication" Language="C#" %>
