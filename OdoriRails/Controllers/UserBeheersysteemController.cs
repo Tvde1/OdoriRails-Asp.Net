e09fcdcc2f8705adb4463d3f7776038388ef76e7
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-05-23 11:06:15 +0200

    Userbeheersysteem controller aangemaakt

---
+++ OdoriRails/Controllers/UserBeheersysteemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdoriRails.Controllers
+{
+    public class UserBeheersysteemController : Controller
+    {
+        // GET: UserBeheersysteem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
