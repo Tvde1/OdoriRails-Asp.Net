ec90b6116262be086b4183e5c64a71b72673a752
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-05-23 10:54:00 +0200

    Huidige aanpassingen logic Userbeheersysteem

---
+++ OdoriRails/Models/UserBeheerSysteemModel.cs
@@ -0,0 +1,13 @@
+﻿using OdoriRails.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdoriRails.Models
+{
+    public class UserBeheerSysteemModel : BaseModel
+    {
+        List<User> users { get; set; }
+    }
+}
