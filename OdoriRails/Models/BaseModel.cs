87e2fdb6fcde0374be5dc96ca4eeebd4e64ea4f5
Tvde1 <tim3345@live.nl>
2017-05-28 16:45:07 +0200

    Added an easy way to set the home screen for every subsystem

--- OdoriRails/Models/BaseModel.cs
+++ OdoriRails/Models/BaseModel.cs
@@ -4,8 +4,21 @@ namespace OdoriRails.Models
 {
     public abstract class BaseModel
     {
-        //Elk model heeft deze
+        //Elk model heeft deze properties
         public User User { get; set; }
+        public string ControllerNameForHomeButton { get; set; }
+
+        public BaseModel(User user, string controllerNameForHomeButton)
+        {
+            User = user;
+            ControllerNameForHomeButton = controllerNameForHomeButton;
+        }
+
+        public BaseModel()
+        {
+            
+        }
+
         public string Error { get; set; }
         public string Sucess { get; set; }
         public string Warning { get; set; }
