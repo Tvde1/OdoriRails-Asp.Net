5193ae5325d28aae617e63c06c71f6f1c19ebde2
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-05-29 11:40:39 +0200

    LogistiekBeheersysteem logic toegevoegd aan project

---
+++ OdoriRails/Helpers/LogistiekBeheersysteem/ObjectClasses/BeheerSector.cs
@@ -0,0 +1,60 @@
+﻿using OdoriRails.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdoriRails.Helpers.LogistiekBeheersysteem
+{
+    public class BeheerSector : Sector
+    {
+        public BeheerSector(int number, int trackNumber, SectorStatus status, Tram tram) : base(number, trackNumber, status, tram.Number)
+        {
+            SetTram(tram);
+        }
+
+        public static BeheerSector ToBeheerSector(Sector sector)
+        {
+            return new BeheerSector(sector.Number, sector.TrackNumber, sector.Status, sector.OccupyingTram);
+        }
+
+        /// <summary>
+        /// Zet de status van de tram naar locked.
+        /// </summary>
+        public void Lock()
+        {
+            Status = SectorStatus.Locked;
+        }
+
+        /// <summary>
+        /// Zet de status van de tram naar open.
+        /// </summary>
+        public void UnLock()
+        {
+            if (OccupyingTram == null)
+            {
+                Status = SectorStatus.Open;
+            }
+            else
+            {
+                Status = SectorStatus.Occupied;
+            }
+        }
+
+        /// <summary>
+        /// Zet de occupying tram. Gebruik `null` om de tram leeg te maken.
+        /// </summary>
+        /// <param name="tram"></param>
+        public bool SetOccupyingTram(Tram tram)
+        {
+            if (Status == SectorStatus.Locked || Status == SectorStatus.Occupied) return false;
+            else
+            {
+                Status = SectorStatus.Occupied;
+                OccupyingTram = tram;
+                return true;
+            }
+
+        }
+    }
+}
---
+++ OdoriRails/Helpers/LogistiekBeheersysteem/ObjectClasses/BeheerTram.cs
@@ -0,0 +1,42 @@
+﻿using OdoriRails.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdoriRails.Helpers.LogistiekBeheersysteem
+{
+    public class BeheerTram : Tram
+    {
+        public BeheerTram(int number, TramStatus status, int line, User driver, TramModel model, TramLocation location, DateTime? departureTime) : base(number, status, line, driver, model, location, departureTime)
+        {
+            Number = number;
+            Status = status;
+            Line = line;
+            Driver = driver;
+            Model = model;
+            Location = location;
+            DepartureTime = departureTime;
+        }
+
+        public static BeheerTram ToBeheerTram(Tram tram)
+        {
+            return new BeheerTram(tram.Number, tram.Status, tram.Line, tram.Driver, tram.Model, tram.Location, tram.DepartureTime);
+        }
+
+        public void EditTramStatus(TramStatus tramStatus)
+        {
+            Status = tramStatus;
+        }
+
+        public void EditTramLocation(TramLocation tramLocation)
+        {
+            Location = tramLocation;
+        }
+
+        public void EditTramDepartureTime(DateTime? departureTime)
+        {
+            DepartureTime = departureTime;
+        }
+    }
+}
