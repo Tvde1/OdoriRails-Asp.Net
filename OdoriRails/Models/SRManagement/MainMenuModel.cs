e434e65c78d91ad200ec6fc6c700529fe36fc344
HIGHPOWEREDTOAS\Pedro <pedrodossantosmarques@gmail.com>
2017-05-30 18:06:06 +0200

    Index changes

--- OdoriRails/Models/SRManagement/MainMenuModel.cs
+++ OdoriRails/Models/SRManagement/MainMenuModel.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using OdoriRails.Controllers;
+using OdoriRails.Helpers.DAL.Repository;
+using OdoriRails.Helpers.Objects;
+
 
 namespace OdoriRails.Models.SRManagement
 {
     public class MainMenuModel
     {
+        private readonly SchoonmaakReparatieRepository _repository = new SchoonmaakReparatieRepository();
+        private User LoggedInUser; // test purposes
+        public List<Repair> RepairList()
+        {
+            List<Repair> replist = new List<Repair>();
+            replist = _repository.GetAllRepairsFromUser(LoggedInUser);
+            return replist;
+        }
+        
+        public List<Cleaning> CleaningList { get; set; }
     }
 }
