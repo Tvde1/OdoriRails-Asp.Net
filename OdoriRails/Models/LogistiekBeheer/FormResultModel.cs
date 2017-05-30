44b1d0f4382311827c04ab2b4f76255a63df98da
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-05-30 13:39:29 +0200

    Edit tabs toegevoegd

--- OdoriRails/Models/LogistiekBeheer/FormResultModel.cs
+++ OdoriRails/Models/LogistiekBeheer/FormResultModel.cs
@@ -8,8 +8,12 @@ namespace OdoriRails.Models
     public class FormResultModel
     {
         public int TramNumber { get; set; }
+        public string TramModel { get; set; }
         public int TrackNumber { get; set; }
+        public string TrackType { get; set; }
         public int SectorNumber { get; set; }
+        public int SectorAmount { get; set; }
+        public int DefaultLine { get; set; }
         public int RadioButton { get; set; }
     }
 }
