54a29404516924647db827a184f25bd5f83284a6
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-05-30 21:21:00 +0200

    Basic fuction aan forms logistiek beheersysteem gemaakt

--- OdoriRails/Controllers/LogistiekBeheersysteemController.cs
+++ OdoriRails/Controllers/LogistiekBeheersysteemController.cs
@@ -12,6 +12,8 @@ namespace OdoriRails.Controllers
 {
     public class LogistiekBeheersysteemController : BaseControllerFunctions
     {
+        LogistiekLogic logic = new LogistiekLogic();
+
         [HttpGet]
         public ActionResult Index(int? id)
         {
@@ -34,6 +36,7 @@ namespace OdoriRails.Controllers
             return View(remise);
         }
 
+        //Tab Actions
         public ActionResult SetStateMain()
         {
             var remise = (LogistiekBeheerModel)Session["Remise"];
@@ -52,6 +55,67 @@ namespace OdoriRails.Controllers
             return RedirectToAction("Index");
         }
 
+        //Main Actions
+        public ActionResult LockTrack(FormResultModel result)
+        {
+            if (result.RadioButton = 1)
+            {
+                logic.Lock(result.TrackNumber, result.SectorNumber);
+            }
+            else
+            {
+                logic.Unlock(result.TrackNumber, result.SectorNumber);
+            }
+            return RedirectToAction("Index");
+        }
 
+        public ActionResult MoveTram(FormResultModel result)
+        {
+            logic.MoveTram(result.TramNumber, result.TrackNumber, result.SectorNumber);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ToggleDisabled(FormResultModel result)
+        {
+            logic.ToggleDisable(result.TramNumber);
+            return RedirectToAction("Index");
+        }
+
+        //Main Actions
+        public ActionResult AddTram(FormResultModel result)
+        {
+            logic.AddTram(result.TramNumber, result.DefaultLine, result.TramModel);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteTram(FormResultModel result)
+        {
+            logic.DeleteTram(result.TramNumber);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddTrack(FormResultModel result)
+        {
+            logic.AddTrack(result.TrackNumber, result.SectorAmount, result.TrackType, result.DefaultLine);
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteTrack(FormResultModel result)
+        {
+            logic.DeleteTram(result.TrackNumber);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddDeleteSector(FormResultModel result)
+        {
+            if(result.RadioButton = 1)
+            {
+                logic.DeleteSector(result.TrackNumber);
+            }
+            else
+            {
+                logic.AddSector(result.TrackNumber);
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
