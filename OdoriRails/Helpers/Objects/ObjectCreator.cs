35e924e3f1f351dfce843439dcfa3181044525ca
Ronald van den Broek <ronald.vandenbroek@live.nl>
2017-06-01 13:42:44 +0200

    Tim test je code VOORDAT je commit en weggaat om döner te eten!

--- OdoriRails/Helpers/Objects/ObjectCreator.cs
+++ OdoriRails/Helpers/Objects/ObjectCreator.cs
@@ -46,9 +46,9 @@ namespace OdoriRails.Helpers.Objects
             if (row == null) return null;
             var array = row.ItemArray;
             var occupyingTramNumber = row["TramFk"] == DBNull.Value ? null : (int?)row["TramFk"];
-            var latitude = (string)row["Latidude"];
-            var longitude = (string)row["Longitude"];
-            return new Sector((int)array[0], (int)array[2], (SectorStatus)array[1], occupyingTramNumber, latitude, longitude);
+           // var latitude = (string)row["Latidude"];
+           // var longitude = (string)row["Longitude"];
+            return new Sector((int)array[0], (int)array[2], (SectorStatus)array[1], occupyingTramNumber, null, null);
         }
 
         public Tram CreateTram(DataRow row)
