023ff511ab1598870b74dbf5d3c4b4ce41b01654
HIGHPOWEREDTOAS\Pedro <pedrodossantosmarques@gmail.com>
2017-06-01 23:21:13 +0200

    Add service classes

--- OdoriRails/Models/SRManagement/AddCleaningModel.cs
+++ OdoriRails/Models/SRManagement/AddCleaningModel.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using OdoriRails.Helpers.Objects;
 
 namespace OdoriRails.Models.SRManagement
 {
-    public class AddCleaningModel
+    public class AddCleaningModel : BaseModel
     {
+        public Dictionary<string, bool> AssignedWorkers { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public CleaningSize Size { get; set; }
+        public string Comment { get; set; }
+        public int TramID { get; set; }
     }
 }
--- OdoriRails/Models/SRManagement/AddRepairModel.cs
+++ OdoriRails/Models/SRManagement/AddRepairModel.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using OdoriRails.Helpers.Objects;
 
 namespace OdoriRails.Models.SRManagement
 {
-    public class AddCleaningModel
+    public class AddRepairModel : BaseModel
     {
+        public Dictionary<string, bool> AssignedWorkers { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public RepairType Size { get; set; }
+        public string Defect { get; set; }
+        public string Solution { get; set; }
+        public int TramID { get; set; }
     }
 }
